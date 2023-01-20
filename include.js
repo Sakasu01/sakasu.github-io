abfbd4ed2dd8feeb6ed2032edffd9296b4ccd069
sakasu <s214229y@st.go.tuat.ac.jp>
2023-01-20 23:48:35 +0900

    update

--- include.js
+++ include.js
@@ -1,8 +1,8 @@
-function include_header(){
+@mixin function include_header(site){
     $.ajax({
-        url: 'header.html', // リクエストを送信するURLを指定
+        url: site+'.html', // リクエストを送信するURLを指定
         async: false, // 非同期リクエストを無効にする
-    }).done(function(header_html){ // 通信が成功したら
-        document.write(header_html); // herder.htmlを表示する
+    }).done(function(site_html){ // 通信が成功したら
+        document.write(site_html); // herder.htmlを表示する
     });
 }
